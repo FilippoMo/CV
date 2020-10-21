e8146ae6ad65a6684895ceed39b50739aef61953
FilippoMo <71963913+FilippoMo@users.noreply.github.com>
2020-10-21 19:38:50 +0200

    Update1-21/10

--- js/hideshow.js
+++ js/hideshow.js
@@ -1,8 +1,14 @@
 function HideShowSkillBars() {
   var x = document.getElementById("skill-bars");
+  var y = document.getElementById("chevronDown");
+  var z = document.getElementById("chevronUp");
   if (x.style.display === "none") {
     x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "inline-block";
   } else {
     x.style.display = "none";
+    y.style.display = "inline-block";
+    z.style.display = "none";
   }
 }
